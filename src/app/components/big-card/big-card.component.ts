657598f0b9ee27e216786880b1180dd2d9d36283
edu-ti <educs85.ti@gmail.com>
2025-03-11 21:14:20 -0300

    content created

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,14 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './big-card.component.css',
 })
 export class BigCardComponent implements OnInit {
-  photoCover: string =
-    'https://static.wikia.nocookie.net/0cb76c54-ba87-4add-b270-0cf0fbd4391c';
-  cardTitle: string = 'NOVA HEROÍNA DA MARVEL';
-  cardDescription: string = `Riri Williams, personagem dos quadrinhos da Marvel, 
-    é quem substituirá Tony Stark como Homem de Ferro. 
-    A série Coração de Ferro (ou Ironheart) 
-    contará a história de Riri Williams, que cria uma 
-    armadura semelhante à do Homem de Ferro.`;
+  @Input()
+  photoCover: string ='';
+
+  @Input()
+  cardTitle: string = '';
+
+  @Input()
+  cardDescription: string = ``;
+
+  @Input()
+  Id:string = "0";
 
   constructor() {}
 
---
+++ src/app/components/data/dataFake.ts
@@ -0,0 +1,121 @@
+export const dataFake = [
+  {
+    id: '1',
+    photo: 'aaaaa',
+    title: 'NOVA HEROÍNA DA MARVEL',
+    description: `Riri Williams, personagem dos quadrinhos da Marvel, 
+        é quem substituirá Tony Stark como Homem de Ferro. 
+        A série Coração de Ferro (ou Ironheart) contará a 
+        história de Riri Williams, que cria uma armadura 
+        semelhante à do Homem de Ferro.`,
+    photoCover:
+      'https://static.wikia.nocookie.net/0cb76c54-ba87-4add-b270-0cf0fbd4391c',
+  },
+
+  {
+    id: '2',
+    photo: 'bbbbb',
+    title: ' NOVO FILME DO HOMEM-ARANHA 2026',
+    description: 'e esse novo filme é muito melhor que o anterior',
+    photoCover:
+      'https://tm.ibxk.com.br/2021/12/16/16192625911681.jpg?ims=1280x480',
+  },
+
+  {
+    id: '3',
+    photo: 'aaaaa',
+    title:
+      'Benedict Cumberbatch volta a falar sobre a presença de Doutor Estranho em ‘Vingadores: Doomsday’',
+    description: `Riri Williams, personagem dos quadrinhos da Marvel, 
+        é quem substituirá Tony Stark como Homem de Ferro. 
+        A série Coração de Ferro (ou Ironheart) contará a 
+        história de Riri Williams, que cria uma armadura 
+        semelhante à do Homem de Ferro.`,
+    photoCover:
+      'https://ingresso-a.akamaihd.net/b2b/production/uploads/article/image/2973/6cf7ea0c30be57ea1e56aaa45ee9b38b.jpg',
+  },
+
+  {
+    id: '4',
+    photo: 'aaaaa',
+    title: 'Thunderbolts* ganha trailer exclusivo durante SDCC',
+    description: `Riri Williams, personagem dos quadrinhos da Marvel, 
+        é quem substituirá Tony Stark como Homem de Ferro. 
+        A série Coração de Ferro (ou Ironheart) contará a 
+        história de Riri Williams, que cria uma armadura 
+        semelhante à do Homem de Ferro.`,
+    photoCover:
+      'https://disneyplusbrasil.com.br/wp-content/uploads/2024/04/Thunderbolts.jpg',
+  },
+
+  {
+    id: '5',
+    photo: 'aaawwaaa',
+    title:
+      'What If…? acerta no melhor substituto para Robert Downey Jr. como Tony Stark',
+    description: `É sabido que Robert Downey Jr. não interpreta mais o Homem de Ferro por motivos óbvios. No entanto, Tony Stark aparece novamente no 6º episódio de What If…?, lançado nesta quarta-feira (15). Ao contrário de outros colegas, o ator não emprestou a voz para reprisar o personagem.
+    O mais novo capítulo da série retorna à longínqua Fase 1 do Universo Cinematográfico Marvel (UCM), mas em um universo alternativo em que Stark nunca foi sequestrado pelos Dez Anéis no Afeganistão – ou seja, ele jamais se torna o Homem de Ferro e os Vingadores nunca se formaram.
+    Pela terceira vez dentro dos 6 episódios já apresentados, a série animada dá um destino cruel para o herdeiro das Indústrias Stark.
+    A decisão da Marvel na hora de escolher o veterano Mick Wingert para viver Tony Stark foi certeira. O dublador consegue imitar muito bem a voz de Robert Downey Jr. e o resultado é bastante notável para o público.
+    O substituto é altamente qualificado, até mesmo porque já interpretou o Homem de Ferro inúmeras vezes em filmes animados e em séries de TV. Para Wingert replicar a voz de Downey não foi um desafio.
+    É interessante recordar que a Marvel cita conflitos de agenda durante a produção de What If…?, por isso houve a decisão de deixar Downey, Chris Evans, Brie Larson e Dave Bautista de molho e escanteio do projeto.
+    O mais provável, no entanto, seja que o ator custe “muito caro” para gravar algumas poucas falas na série animada depois que seu longo contrato com a Marvel chegou ao fim.
+    Sobre What If…?
+    Um dos projetos mais interessantes apresentados pela Marvel para a Fase 4 é a série animada denominada What If…?, ou E Se…?.
+    Houveram pequenas séries parecidas nos quadrinhos, que lidavam com possibilidades que nunca existiram no cânone, mas poderiam ter acontecido. Tudo era observado por Uatu, o Vigia, que fará o papel do narrador por aqui.
+    A série promete mostrar diversas possibilidades que acabaram não acontecendo no Universo Cinematográfico Marvel, mas poderemos matar a curiosidade.`,
+    photoCover:
+      'https://www.einerd.com/wp-content/uploads/2021/09/what-if-tony-stark-890x606.jpg',
+  },
+
+  {
+    id: '6',
+    photo: 'aaasaa',
+    title: 'Heróis da Marvel retornam para Vingadores: Guerra Infinita',
+    description: `O filme "Vingadores: Guerra Infinita" reúne mais de 20 super-heróis da Marvel e estreia amanhã, dia 26 de abril.
+    Mais de 20 super-heróis em um combate contra um titã genocida que deseja destruir metade do universo: que a guerra comece e a bilheteria comprove o sucesso.
+    "Vingadores: Guerra Infinita" estreia esta semana nos Estados Unidos e no Brasil, 10 anos depois do primeiro filme da franquia mais lucrativa do cinema: "Homem de Ferro".
+    E Robert Downey Jr. retorna ao traje do Homem de Ferro para combater, ao lado de seus colegas, o impiedoso Thanos (Josh Brolin), que pretende destruir metade do universo 
+    com a alegação de que assim formará um equilíbrio.`,
+    photoCover:
+      'https://classic.exame.com/wp-content/uploads/2018/04/vingadores.jpg?quality=70&strip=info&w=1024',
+  },
+
+  {
+    id: '7',
+    photo: 'awaaaa',
+    title:
+      'Marvel Comics anuncia seus novos talentos exclusivos stormbreakers de 2023',
+    description: `A Marvel Comics anunciou hoje seu novo time de artistas 'Stormbreakers' 
+    da Turma de 2023, o principal programa de distinção de talentos da empresa que destaca a 
+    próxima geração de artistas de elite da indústria. A turma de Stormbreakers 2023 da Marvel 
+    durará dois anos, com muitos dos nomes trabalhando nos maiores títulos da biblioteca da 
+    Marvel hoje sob contratos exclusivos. Os criadores que compõem a nova classe incluem 
+    Elena Casagrande (Viúva Negra), Nic Klein (Thor), Jan Bazaldúa (Legion of X), 
+    Chris Allen (Miles Morales), Martin Coccolo (Deadpool), Lucas Werneck (Immortal X-Men), 
+    C. F. Villa (X-Men) e Federico Vicentini (X Mortes de Wolverine).
+    Lançado pela primeira vez em 2020, o Stormbreakers da Marvel é a próxima 
+    evolução do programa Young Guns da Marvel. Nos últimos 15 anos, a Marvel selecionou e 
+    reconheceu 36 artistas promissores que desenharam alguns dos maiores eventos da empresa, 
+    séries icônicas e muito mais, tornando-se nomes favoritos dos fãs na indústria, incluindo 
+    Steve McNiven, Jim Cheung, Sara Pichelli, Ryan Stegman e muito mais. Os Stormbreakers da 
+    arvel continua a tradição que o Young Guns da Marvel estabeleceu e reconhece seus ex-alunos 
+    hoje como Stormbreakers honorários, além de seu trabalho como Young Guns.`,
+    photoCover:
+      'https://disneyplusbrasil.com.br/wp-content/uploads/2024/04/Thunderbolts.jpg',
+  },
+
+  {
+    id: '8',
+    photo: 'adsaaaa',
+    title: 'Os 10 piores filmes da Marvel, segundo a crítica',
+    description: `A Marvel Studios conquistou o coração de milhões de fãs ao 
+    redor do mundo com seu vasto universo cinematográfico repleto de super-heróis 
+    icônicos e tramas emocionantes. No entanto, nem todos os filmes do estúdio foram 
+    recebidos com o mesmo entusiasmo pela crítica. 
+    Por isso, elencamos aqui os 10 piores filmes da Marvel, segundo a crítica especializada, 
+    examinando suas falhas e desafios.`,
+    photoCover:
+      'https://img.odcdn.com.br/wp-content/uploads/2023/12/imagem_2023-12-19_013932059.png',
+  },
+];
